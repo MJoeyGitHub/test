c8fc88c8531ad0542e21efef69ece1f84cd92c26
Joey <17710764656@163.com>
2017-04-26 15:08:55 +0800

    听说这个是注释？

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Joey on 2017/4/26.
+//  Copyright © 2017年 Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Joey on 2017/4/26.
+//  Copyright © 2017年 Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
